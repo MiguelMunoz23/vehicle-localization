b6ea411951273b30d119e9be2e954349d88a546c
miguel <miguel.munoz@udem.edu>
2022-08-28 22:22:20 -0500

    Car now goes along the x axis and rotates.
    + Added a Class "Car" to control the position and orientation of the car.
    + Main loop to check keys pressed.

--- vehicle-localization.py
+++ vehicle-localization.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3.8
-
 """
 vehicle-localization.py
 
@@ -33,23 +31,40 @@ BLACK = (0, 0, 0)
 class Car:
     CAR_WIDTH = 40
     CAR_HEIGHT = 20
+    VEL = 1  # 100 km/h
+    ANGLE_STEP = 1.2
 
     def __init__(self):
-        self.image = pygame.transform.scale(pygame.image.load('car.png'), (self.CAR_WIDTH, self.CAR_HEIGHT))
         self.x = 0
         self.y = 0
-        self.vel = 5  # 100 km/h
+        self.angle = 0
+        self.image = None
 
     def draw(self, window):
+        self.image = pygame.transform.rotate(
+            pygame.transform.scale(pygame.image.load('car.png'), (self.CAR_WIDTH, self.CAR_HEIGHT)), self.angle)
         # Display car image in the center of the screen and add the x and y coordinate
         window.blit(self.image, (WIDTH // 2 - self.CAR_WIDTH // 2 + self.x,
                                  HEIGHT // 2 - self.CAR_HEIGHT // 2 + self.y))
 
-    def move(self, up):
+    def move(self, up=None, up_left=None, up_right=None, down=None, down_left=None, down_right=None):
+        # Check every condition and move the car
         if up:
-            self.x += self.vel
-        else:
-            self.x -= self.vel
+            self.x += self.VEL
+        if up_left:
+            self.x += self.VEL
+            self.angle += self.ANGLE_STEP
+        if up_right:
+            self.x += self.VEL
+            self.angle -= self.ANGLE_STEP
+        if down:
+            self.x -= self.VEL
+        if down_left:
+            self.x -= self.VEL
+            self.angle += self.ANGLE_STEP
+        if down_right:
+            self.x -= self.VEL
+            self.angle -= self.ANGLE_STEP
 
 
 def draw(window, car):
@@ -59,10 +74,19 @@ def draw(window, car):
 
 
 def handle_movement(car, keys):
+    # Check which keys are being pressed
     if keys[pygame.K_UP]:
         car.move(up=True)
+    if keys[pygame.K_LEFT] and keys[pygame.K_UP]:
+        car.move(up_left=True)
+    if keys[pygame.K_RIGHT] and keys[pygame.K_UP]:
+        car.move(up_right=True)
     if keys[pygame.K_DOWN]:
-        car.move(up=False)
+        car.move(down=True)
+    if keys[pygame.K_LEFT] and keys[pygame.K_DOWN]:
+        car.move(down_left=True)
+    if keys[pygame.K_RIGHT] and keys[pygame.K_DOWN]:
+        car.move(down_right=True)
 
 
 def main():
