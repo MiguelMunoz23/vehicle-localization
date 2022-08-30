c3db6049a0c160570cfa53cdfb48d4657a1becc5
miguel <miguel.munoz@udem.edu>
2022-08-30 14:24:50 -0500

    + Vehicle now moves at the right speed when turning. + Vehicle now leaves a trail based on the center of the image's bounding box.

--- vehicle-localization.py
+++ vehicle-localization.py
@@ -19,6 +19,7 @@ from math import sin, cos
 import pygame
 import argparse
 from numpy import deg2rad
+import time
 
 # Initialize pygame and constants
 pygame.init()
@@ -31,68 +32,88 @@ pygame.display.set_caption("Ackerman Steering Simulator | Advanced Robotics")
 
 # Define colors
 BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+GREEN = (0, 255, 0)
 
 
 class Car:
-    CAR_WIDTH = 40
-    CAR_HEIGHT = 20
     VEL = 1  # 100 km/h
-    ANGLE_STEP = 0.7
+    ANGLE_STEP = 1.2
 
     def __init__(self):
+        self.width = 40
+        self.height = 20
         self.x = 0
         self.y = 0
+        self.past_positions = []
+        self.car_x_center = WIDTH // 2 - self.width // 2
+        self.car_y_center = HEIGHT // 2 - self.height // 2
         self.angle = 0
+        self.x_vel = 0
+        self.y_vel = 0
         self.image = None
+        self.bounding_box = None
 
     def draw(self, window):
+        # Rotate the image with respect to the angle
         self.image = pygame.transform.rotate(
-            pygame.transform.scale(pygame.image.load('car.png'), (self.CAR_WIDTH, self.CAR_HEIGHT)), self.angle)
+            pygame.transform.scale(pygame.image.load('car.png'), (self.width, self.height)), self.angle)
+        # Bounding box
+        self.bounding_box = self.image.get_bounding_rect()
+        self.bounding_box.center = self.car_x_center + self.x + self.image.get_width()//2,\
+                                   self.car_y_center + self.y + self.image.get_height()//2
+        pygame.draw.rect(window, GREEN, self.bounding_box, 1)
+        # Code for leaving trail
+        self.past_positions.append([self.bounding_box.center[0], self.bounding_box.center[1]])
+        for point in self.past_positions:
+            pygame.draw.circle(window, YELLOW, point, 2)
         # Display car image in the center of the screen and add the x and y coordinate
-        window.blit(self.image, (WIDTH // 2 - self.CAR_WIDTH // 2 + self.x,
-                                 HEIGHT // 2 - self.CAR_HEIGHT // 2 + self.y))
+        window.blit(self.image, (self.car_x_center + self.x, self.car_y_center + self.y))
 
     def move(self, up=None, up_left=None, up_right=None, down=None, down_left=None, down_right=None):
         # Check every condition and move the car accordingly
+        self.x_vel = self.VEL * cos(deg2rad(self.angle))
+        self.y_vel = self.VEL * sin(deg2rad(self.angle))
         if up:  # Forward
-            self.x += self.VEL * cos(deg2rad(self.angle))
-            self.y -= self.VEL * sin(deg2rad(self.angle))
+            self.x += self.x_vel
+            self.y -= self.y_vel
         if up_left:  # Forward left
-            self.x += self.VEL * cos(deg2rad(self.angle))
-            self.y -= self.VEL * sin(deg2rad(self.angle))
+            self.x += self.x_vel
+            self.y -= self.y_vel
             self.angle += self.ANGLE_STEP
         if up_right:  # Forward right
-            self.x += self.VEL * cos(deg2rad(self.angle))
-            self.y -= self.VEL * sin(deg2rad(self.angle))
+            self.x += self.x_vel
+            self.y -= self.y_vel
             self.angle -= self.ANGLE_STEP
         if down:  # Backwards
-            self.x -= self.VEL * cos(deg2rad(self.angle))
-            self.y += self.VEL * sin(deg2rad(self.angle))
+            self.x -= self.x_vel
+            self.y += self.y_vel
         if down_left:  # Backwards left
-            self.x -= self.VEL * cos(deg2rad(self.angle))
-            self.y += self.VEL * sin(deg2rad(self.angle))
+            self.x -= self.x_vel
+            self.y += self.y_vel
             self.angle -= self.ANGLE_STEP
         if down_right:  # Backwards right
-            self.x -= self.VEL * cos(deg2rad(self.angle))
-            self.y += self.VEL * sin(deg2rad(self.angle))
+            self.x -= self.x_vel
+            self.y += self.y_vel
             self.angle += self.ANGLE_STEP
 
 
 def draw(window, car):
-    window.fill(BLACK)
+    WINDOW.fill(BLACK)
     car.draw(window)
     pygame.display.update()
 
 
 def handle_movement(car, keys):
     # Check which keys are being pressed
-    if keys[pygame.K_UP]:
+    if keys[pygame.K_UP] and not(keys[pygame.K_LEFT] or keys[pygame.K_RIGHT]):
         car.move(up=True)
     if keys[pygame.K_LEFT] and keys[pygame.K_UP]:
         car.move(up_left=True)
     if keys[pygame.K_RIGHT] and keys[pygame.K_UP]:
         car.move(up_right=True)
-    if keys[pygame.K_DOWN]:
+    if keys[pygame.K_DOWN] and not(keys[pygame.K_LEFT] or keys[pygame.K_RIGHT]):
         car.move(down=True)
     if keys[pygame.K_LEFT] and keys[pygame.K_DOWN]:
         car.move(down_left=True)
