c47790ce3f2d704b0d1e87772dfed579c2cf5db9
miguel <miguel.munoz@udem.edu>
2022-08-31 00:02:07 -0500

    + Added text to the window including the config parameters and the x, y, angle of the car. + Implemented increment and decrement of speed using "f" and "s" keys, respectively. + Tried to implement equations. Need to work on that.

--- vehicle-localization.py
+++ vehicle-localization.py
@@ -14,7 +14,7 @@ Date of creation: 28 August 2022
 """
 
 import argparse
-from math import sin, cos
+from math import sin, cos, atan, tan
 
 # Import useful libraries
 import pygame
@@ -22,10 +22,11 @@ from numpy import deg2rad
 
 # Initialize pygame and constants
 pygame.init()
-FPS = 60
+FPS = 120
+FONT = pygame.font.Font(None, 20)
 
 # Create a window
-WIDTH, HEIGHT = 1000, 1000
+WIDTH, HEIGHT = 800, 800
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Ackerman Steering Simulator | Advanced Robotics")
 
@@ -37,8 +38,11 @@ GREEN = (0, 255, 0)
 
 
 class Car:
-    VEL = 1  # 100 km/h
-    ANGLE_STEP = 1.2
+    VEL = 1  # 100 [km/h]
+    ANGLE_STEP = 1.2  # [°]
+    DELTA_T = 0.1  # [sec]
+    LF = 1.4  # [m]
+    LB = 1.2  # [m]
 
     def __init__(self):
         self.width = 40
@@ -52,7 +56,10 @@ class Car:
         self.x_vel = 0
         self.y_vel = 0
         self.image = None
-        self.bounding_box = None
+        self.beta_k = 0
+        self.x_k = 0
+        self.y_k = 0
+        self.phi = 0
 
     def draw(self, window):
         self.x_pos = WIDTH // 2 + self.x
@@ -70,7 +77,8 @@ class Car:
         # Display car image in the center of the screen and add the x and y coordinate
         window.blit(self.image, self.image.get_rect(center=(self.x_pos, self.y_pos)))
 
-    def move(self, up=None, up_left=None, up_right=None, down=None, down_left=None, down_right=None):
+    def move(self, up=None, up_left=None, up_right=None, down=None, down_left=None, down_right=None, speed_up=None,
+             speed_down=None):
         # Check every condition and move the car accordingly
         self.x_vel = self.VEL * cos(deg2rad(self.angle))
         self.y_vel = self.VEL * sin(deg2rad(self.angle))
@@ -96,11 +104,45 @@ class Car:
             self.x -= self.x_vel
             self.y += self.y_vel
             self.angle += self.ANGLE_STEP
+        if speed_up:
+            self.VEL += 0.1
+        if speed_down:
+            self.VEL -= 0.1
+
+    def print_parameters(self, window):
+        texts = ["CONFIG. PARAMS",
+                 f"Speed: {(self.VEL*100):.2f} km/h",
+                 f"\u0394T: {self.DELTA_T} s",
+                 f"\u0394f increment: {self.ANGLE_STEP} °",
+                 f"Lb: {self.LB} m",
+                 f"Lf: {self.LF} m"]
+        text_x, text_y = 20, 20
+        i = 0
+        for text in texts:
+            text_rendered = FONT.render(text, True, WHITE)
+            window.blit(text_rendered, (text_x, text_y + i))
+            if i == 0:
+                i += 20
+            i += 15
+
+    def apply_equations(self):
+        self.beta_k = atan(self.LB * tan(self.angle) / (self.LF + self.LB))
+        self.x_k = self.x + self.VEL * self.DELTA_T * cos(self.angle + self.beta_k)
+        self.y_k = self.y + self.VEL * self.DELTA_T * cos(self.angle + self.beta_k)
+        self.phi = self.phi + self.VEL * self.DELTA_T * (cos(self.beta_k) * tan(self.angle) / (self.LF + self.LB))
+
+    def print_position(self, window):
+        self.apply_equations()
+        position_str = f"({self.x_k:.3f} m, {self.y_k:.3f} m, {self.phi:.3f} °)"
+        position_rendered = FONT.render(position_str, True, GREEN)
+        window.blit(position_rendered, (self.x_pos + 15, self.y_pos + 15))
 
 
 def draw(window, car):
     WINDOW.fill(BLACK)
     car.draw(window)
+    car.print_parameters(window)
+    car.print_position(window)
     pygame.display.update()
 
 
@@ -118,6 +160,12 @@ def handle_movement(car, keys):
         car.move(down_left=True)
     if keys[pygame.K_RIGHT] and keys[pygame.K_DOWN]:
         car.move(down_right=True)
+    if keys[pygame.K_f] and car.VEL <= 3 and (keys[pygame.K_UP] or keys[pygame.K_DOWN] or
+                                              keys[pygame.K_LEFT] or keys[pygame.K_RIGHT]):
+        car.move(speed_up=True)
+    if keys[pygame.K_s] and car.VEL > 0.1 and (keys[pygame.K_UP] or keys[pygame.K_DOWN] or
+                                               keys[pygame.K_LEFT] or keys[pygame.K_RIGHT]):
+        car.move(speed_down=True)
 
 
 def main():
