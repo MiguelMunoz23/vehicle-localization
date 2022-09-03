a9e8a2046a0166af715bd941d17cdae1ed599e3a
miguel <miguel.munoz@udem.edu>
2022-09-03 17:07:34 -0500

    Car now moves relative to the velocity computed with the equations (got rid of unnecessary variables)

--- vehicle-localization.py
+++ vehicle-localization.py
@@ -36,16 +36,22 @@ WHITE = (255, 255, 255)
 SKY_BLUE = (0, 170, 228)
 GREEN = (0, 255, 0)
 
+# Define speed limits
+UPPER_SPEED_LIMIT = 300
+LOWER_SPEED_LIMIT = 0
+
 
 class Car:
     ANGLE_STEP = 1.2  # [°]
+    SPEED_STEP = 2  # [km/h]
+    METERS_PER_PIXEL = 2  # 1 px = 2 mts
 
     def __init__(self, vehicle_speed, lf, lb, x0, y0, phi0, df0, dt):
-        self.vel, self.vel_k_1 = vehicle_speed / 100, 0  # 1 = 100 [km/h]
+        self.vel, self.vel_k_1 = vehicle_speed, 0  # 1 = 100 [km/h]
         self.lf = lf
         self.lb = lb
-        self.x_pos = x0
-        self.y_pos = y0
+        self.x_k, self.x_k_1 = x0, x0
+        self.y_k, self.y_k_1 = y0, y0
         self.phi, self.phi_k_1 = phi0, phi0
         self.delta_k, self.delta_k_1 = df0, df0
         self.delta_t = dt
@@ -53,29 +59,25 @@ class Car:
         self.image = None
         self.width = 40
         self.height = 20
-        self.x = 0
-        self.y = 0
-        self.angle = 0
+        self.x_pos = 0
+        self.y_pos = 0
         self.past_positions = []
-        self.x_vel = 0
-        self.y_vel = 0
-
         self.beta_k_1 = 0
-        self.x_k, self.x_k_1 = 0, 0
-        self.y_k, self.y_k_1 = 0, 0
 
     def draw(self, window):
-        self.x_pos = WIDTH // 2 + self.x
-        self.y_pos = HEIGHT // 2 + self.y
+        self.x_pos = WIDTH // 2 + self.x_k / self.METERS_PER_PIXEL
+        self.y_pos = HEIGHT // 2 + self.y_k * -1 / self.METERS_PER_PIXEL
 
         # Rotate the image with respect to the angle
         self.image = pygame.transform.rotate(
-            pygame.transform.scale(pygame.image.load('car.png'), (self.width, self.height)), self.angle)
+            pygame.transform.scale(pygame.image.load('car.png'), (self.width, self.height)), rad2deg(self.phi))
 
         # Leaving trail
         self.past_positions.append([self.x_pos, self.y_pos])
         for point in self.past_positions:
             pygame.draw.circle(window, SKY_BLUE, point, 2)
+        # Draw center of the screen
+        pygame.draw.circle(window, WHITE, (WIDTH//2, HEIGHT//2), 5)
 
         # Display car image in the center of the screen and add the x and y coordinate
         window.blit(self.image, self.image.get_rect(center=(self.x_pos, self.y_pos)))
@@ -84,45 +86,31 @@ class Car:
              speed_down=None, not_moving=None):
         self.vel_k_1 = self.vel
         # Check every condition and move the car accordingly
-        self.x_vel = self.vel * cos(deg2rad(self.angle))
-        self.y_vel = self.vel * sin(deg2rad(self.angle))
         if up:  # Forward
-            self.x += self.x_vel
-            self.y -= self.y_vel
             self.delta_k = 0
         if up_left:  # Forward left
-            self.x += self.x_vel
-            self.y -= self.y_vel
             self.delta_k = self.ANGLE_STEP
         if up_right:  # Forward right
-            self.x += self.x_vel
-            self.y -= self.y_vel
             self.delta_k = self.ANGLE_STEP * -1
         if down:  # Backwards
-            self.x -= self.x_vel
-            self.y += self.y_vel
             self.delta_k = 0
             self.vel_k_1 *= -1
         if down_left:  # Backwards left
-            self.x -= self.x_vel
-            self.y += self.y_vel
             self.delta_k = self.ANGLE_STEP * -1
             self.vel_k_1 *= -1
         if down_right:  # Backwards right
-            self.x -= self.x_vel
-            self.y += self.y_vel
             self.delta_k = self.ANGLE_STEP
             self.vel_k_1 *= -1
         if speed_up:
-            self.vel += 0.05
+            self.vel += self.SPEED_STEP
         if speed_down:
-            self.vel -= 0.05
+            self.vel -= self.SPEED_STEP
         if not_moving:
             self.vel_k_1 = 0
 
     def print_parameters(self, window):
         texts = ["CONFIG. PARAMS",
-                 f"Speed: {(self.vel*100):.2f} km/h",
+                 f"Speed: {self.vel:.2f} km/h",
                  f"\u0394T: {self.delta_t} s",
                  f"\u0394f increment: {self.ANGLE_STEP} °",
                  f"Lb: {self.lb} m",
@@ -141,15 +129,15 @@ class Car:
         self.x_k_1 = self.x_k
         self.y_k_1 = self.y_k
         self.phi_k_1 = self.phi
-        self.angle = rad2deg(self.phi)
 
         self.beta_k_1 = atan2((self.lb * tan(deg2rad(self.delta_k_1))), (self.lf + self.lb))
-        self.x_k = self.x_k_1 + 100/3.6 * self.vel_k_1 * self.delta_t * cos(self.phi_k_1 + self.beta_k_1)
-        self.y_k = self.y_k_1 + 100/3.6 * self.vel_k_1 * self.delta_t * sin(self.phi_k_1 + self.beta_k_1)
-        self.phi = (self.phi_k_1 + 100/3.6 * self.vel_k_1 * self.delta_t * cos(self.beta_k_1) * tan(
+        self.x_k = self.x_k_1 + self.vel_k_1/3.6 * self.delta_t * cos(self.phi_k_1 + self.beta_k_1)
+        self.y_k = self.y_k_1 + self.vel_k_1/3.6 * self.delta_t * sin(self.phi_k_1 + self.beta_k_1)
+        self.phi = (self.phi_k_1 + self.vel_k_1/3.6 * self.delta_t * cos(self.beta_k_1) * tan(
             deg2rad(self.delta_k_1)) / (self.lf + self.lb)) % (2 * pi)
+
         print(f"Beta: {rad2deg(self.beta_k_1):.3f}°, x: {self.x_k:.3f} m, y: {self.y_k:.3f} m, "
-              f"heading_angle: {rad2deg(self.phi):.4f}")
+              f"heading_angle: {rad2deg(self.phi):.3f}°")
 
     def print_position(self, window):
         self.apply_equations()
@@ -168,23 +156,26 @@ def draw(window, car):
 
 def handle_movement(car, keys):
     # Check which keys are being pressed
-    if keys[pygame.K_UP] and not (keys[pygame.K_LEFT] or keys[pygame.K_RIGHT]):
+    moving = False
+    if keys[pygame.K_UP] or keys[pygame.K_DOWN] or keys[pygame.K_LEFT] or keys[pygame.K_RIGHT]:
+        moving = True
+    if keys[pygame.K_UP] and not (keys[pygame.K_DOWN] or keys[pygame.K_LEFT] or keys[pygame.K_RIGHT]):
         car.move(up=True)
     if keys[pygame.K_LEFT] and keys[pygame.K_UP] and not keys[pygame.K_RIGHT]:
         car.move(up_left=True)
     if keys[pygame.K_RIGHT] and keys[pygame.K_UP] and not keys[pygame.K_LEFT]:
         car.move(up_right=True)
-    if keys[pygame.K_DOWN] and not (keys[pygame.K_LEFT] or keys[pygame.K_RIGHT]):
+    if keys[pygame.K_DOWN] and not (keys[pygame.K_UP] or keys[pygame.K_LEFT] or keys[pygame.K_RIGHT]):
         car.move(down=True)
     if keys[pygame.K_LEFT] and keys[pygame.K_DOWN] and not keys[pygame.K_RIGHT]:
         car.move(down_left=True)
     if keys[pygame.K_RIGHT] and keys[pygame.K_DOWN] and not keys[pygame.K_LEFT]:
         car.move(down_right=True)
-    if keys[pygame.K_f] and car.vel < 2.99 and (keys[pygame.K_UP] or keys[pygame.K_DOWN]):
+    if keys[pygame.K_f] and car.vel < UPPER_SPEED_LIMIT and moving:
         car.move(speed_up=True)
-    if keys[pygame.K_s] and car.vel > 0.01 and (keys[pygame.K_UP] or keys[pygame.K_DOWN]):
+    if keys[pygame.K_s] and car.vel > LOWER_SPEED_LIMIT and moving:
         car.move(speed_down=True)
-    if not (keys[pygame.K_UP] or keys[pygame.K_DOWN] or keys[pygame.K_LEFT] or keys[pygame.K_RIGHT]):
+    if not moving:
         car.move(not_moving=True)
 
 
